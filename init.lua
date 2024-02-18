26fc43852bad8e43b42494f2767a7dcbec2f6fce
John Ladan <john@ladan.ca>
2024-02-18 11:25:14 -0500

    fix powershell config so that :%!jq . works

--- init.lua
+++ init.lua
@@ -7,7 +7,14 @@ vim.g.maplocalleader = ' '
 -- Set shell on windows
 -- alternatively, use vim.fn.has('macunix'), but cannot find in helpfiles
 if vim.loop.os_uname().sysname == 'Windows_NT' then
+    -- see :h shell-powershell
     vim.go.shell = "powershell.exe"
+    -- vim.go.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
+    vim.go.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';'
+    vim.go.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    vim.go.shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+    vim.go.shellquote= ''
+    vim.go.shellxquote=''
 end
 
 -- [[ Install `lazy.nvim` plugin manager ]]
