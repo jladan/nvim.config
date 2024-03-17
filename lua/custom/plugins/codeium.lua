174ff40437763a0b46024dc72dd728b856627520
John Ladan <john.ladan@vretta.com>
2024-03-17 11:29:43 -0400

    more distinct binding for codeium

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,3 +1,12 @@
+vim.g.codeium_disable_bindings = 1
+
+
+-- Remove the `use` here if you're using folke/lazy.nvim.
 return {
-    'Exafunction/codeium.vim'
+    'Exafunction/codeium.vim',
+    config = function ()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        vim.keymap.set('i', '<c-w>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    end
 }
